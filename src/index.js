44af735903d04e6629c98a70b99971a7a64787cc
bhuwan-acharya <bhuwanacharya56@gmail.com>
2021-02-07 12:59:47 +0100

    dotenv

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { AppProvider } from "./context";
 import { BrowserRouter as Router } from "react-router-dom";
+require("dotenv").config();
 ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
